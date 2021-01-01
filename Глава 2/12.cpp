cdf4e3b6caaf93b43bb7fa0bdacccfac63ea00d7
trojninalex <55929425+aitron-dev@users.noreply.github.com>
2021-01-01 22:18:31 +0300

    Глава 2 завершена

--- Глава 2/12.cpp
+++ Глава 2/12.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <locale>
 
 int main() {
-  
+  //std::locale::global(std::locale("ru_RU.UTF-8"));
+  std::wcout << "Введите число десятичных фунтов: ";
+  float money;
+  std::cin >> money;
 
+  int  funt = 0, shilling = 0, pence = 0;
+  float decfrac;
+  funt = static_cast<int>(money);
+  decfrac = money - funt;
+
+  shilling = decfrac * 20;
+  pence =  (decfrac* 20 - shilling) * 12;
+
+  std::wcout << "Эквивалентная сумма в старой форме записи: \u00A3" << funt << '.' << shilling << '.' << pence << std::endl;
   return 0;
 }
